ed0939178575ee138e5eb5dd77250d36cd602120
Meshack Mbuvi <meshmbuvi@gmail.com>
2024-05-26 22:26:19 +0300

    Determine factorial of a number.

---
+++ math_algorithms/factorial.js
@@ -0,0 +1,18 @@
+/**
+ * Problem: GIven an integer n, find the factorial of that integer.
+ * Definition: A factorial is a non-negative number n denoted n! which is the
+ * product of all positive numbers less than or equal to n.
+ * 0! = 0
+ * 1! = 1
+ * 2! = 2
+ * 3! = 1*2*3
+ */
+
+function factorial(n) {
+  if (n < 2) return n;
+
+  return n * factorial(n - 1);
+}
+
+console.log("4!", factorial(4));
+console.log("5!", factorial(5));
