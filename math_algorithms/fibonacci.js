456127c284f148947dc6f052c33c5d9affb04182
Meshack Mbuvi <meshmbuvi@gmail.com>
2024-05-26 22:12:48 +0300

    Add implementation for fibonacci sequence.

---
+++ math_algorithms/fibonacci.js
@@ -0,0 +1,18 @@
+/*
+ * Problem: Given a number n, find the first 'n' elements of the fibonacci sequence
+ * Definition: It is a sequence in which each number is the sum of the two preceding ones.
+ **/
+function fibonacci(n) {
+  let fib = [0, 1];
+
+  for (let i = 2; i < n; i++) {
+    fib[i] = fib[i - 1] + fib[i - 2];
+  }
+
+  return fib;
+}
+
+console.log("Fib(1)", fibonacci(1));
+console.log("Fib(3)", fibonacci(3));
+console.log("Fib(5)", fibonacci(5));
+console.log("Fib(7)", fibonacci(7));
